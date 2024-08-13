cb048fef59ef9ca72bd53cb3574aaeac12794880
lyn <liuyanan113@163.com>
2024-08-13 22:01:28 +0800

    struct

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-// 整数 对应的二进制输出
-struct Node {
-    char name[10];
-    short age;
-    double money;
+
+struct Bit {
+    unsigned char bit0: 1;
+    unsigned char bit1: 1;
+    unsigned char bit2: 1;
+    unsigned char bit3: 1;
+    unsigned char bit4: 1;
+    unsigned char bit5: 1;
+    unsigned char bit6: 1;
+    unsigned char bit7: 1;
 };
-//最大的基本数据类型 double
-
-
 int main() {
-    struct Node n = {"wonderful", 37, 18.85};
-    printf("结构体 - 首地址: %p\n", &n);
-    printf("结构体 - 首元素: %p\n", &n.name);
-    printf("结构体二元素地址: %p\n", &n.age);
-    printf("结构体三元素地址: %p\n", &n.money);
 
-    printf("%lu\n", sizeof(n));
-
+    unsigned num = 0x12345678;
+    printf("%lu\n", sizeof(num));
+    struct Bit *p = (struct Bit *) &num;
+    for (int i = 0; i < sizeof(num); ++i) {
+        struct Bit *pBit = p + i;
+        printf("%d%d%d%d%d%d%d%d ", pBit->bit0, pBit->bit1, pBit->bit2, pBit->bit3, pBit->bit4, pBit->bit5, pBit->bit6, pBit->bit7);
+    }
     return 0;
 }
